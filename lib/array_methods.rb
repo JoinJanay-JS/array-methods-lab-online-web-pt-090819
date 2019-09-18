3148627c2c564da7e95b391617006cf47c14370b
Janay Bailey <tjanay87@gmail.com>
2019-09-18 17:36:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,16 +14,16 @@ array.reverse()
 end
 
 def using_first(array)
-array = ["wow", "I'm", "really", "learning", "these", "arrays!"]
+array = ("wow", "I'm", "really", "learning", "these", "arrays!")
 array.first()
 end
 
 def using_last(array)
-array = ["wow", "i'm", "really", "learning", "these", "arrays!"]
+array = ("wow", "i'm", "really", "learning", "these", "arrays!")
 array.last()
 end
 
 def using_size(array)
-  array =  ["wow", "i'm", "really", "learning", "these", "arrays!"]
+  array =  ("wow", "i'm", "really", "learning", "these", "arrays!")
 array.size()
 end
